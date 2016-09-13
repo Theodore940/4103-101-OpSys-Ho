5e2c02f2d3447aadd8bd5b32ddff51740ed458fc
Nathan Ho <Nathanho0628@gmail.com>
2016-09-13 01:24:38 -0500

    adding homework 1

--- assignments/homework-01/sum_args.sh
+++ assignments/homework-01/sum_args.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+t=0
 for i in $@
 do
+	var2=$(expr $# - 1)
 	sum=$(expr $sum + $i)
+	echo -n $i 
+	if [ $t -lt $var2 ]
+	then
+	echo -n +
+	t=$((t+1))
+	continue
+	fi
 done
-
-echo  $@ = $sum
+	echo -n =$sum
