c5c367975efb016f1372a08031692af8f495b39b
Nathan Ho <Nathanho0628@gmail.com>
2016-09-12 21:23:31 -0500

    Revert "homework"
    This reverts commit 9e7a55f6749df72268107e43908bc70f3262d022.

--- assignments/homework-01/myrandom.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-dictionary=/usr/share/dict/words
-
-lines=$(cat $dictionary | wc -1)
-
-echo $lines
--- assignments/homework-01/sum_args.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in $@
-do
-	sum=$(expr $sum + $i)
-done
-
-echo $sum
